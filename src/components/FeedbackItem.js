10b40e6a9b99cb8a0508434cd7771ff15d90a998
Helena Johansson <helena@klarr.se>
2022-05-22 18:44:02 +0200

    code from part3

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
+import Card from "./Card";
 
 const FeedbackItem = ({ item }) => {
   //const [rating, setRating] = useState(8);
   //const [text, setText] = useState("This is an example of a feedback item");
   return (
-    <div className="card">
+    <Card>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   );
 };
 
