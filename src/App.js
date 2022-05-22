450adfdf3909d53b6daff660e51455c7854ddf84
Helena Johansson <helena@klarr.se>
2022-05-22 18:23:37 +0200

    code from part2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import FeedbackData from "./data/FeedbackData";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -13,7 +13,11 @@ function App() {
       <Header />
       <div className="container">
         <Routes>
-          <Route exact path="/" element={<FeedbackItem />} />
+          <Route
+            exact
+            path="/"
+            element={<FeedbackList feedback={feedback} />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
