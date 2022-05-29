fbca6de59c17c1fc76bdad7f29d6c78f6f399c32
Helena Johansson <helena@klarr.se>
2022-05-29 16:20:33 +0200

    part 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 import FeedbackData from "./data/FeedbackData";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+      // will return an array minus the one we are deleleting
+    }
+  };
 
   return (
     <BrowserRouter>
@@ -16,7 +25,15 @@ function App() {
           <Route
             exact
             path="/"
-            element={<FeedbackList feedback={feedback} />}
+            element={
+              <>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
           />
         </Routes>
       </div>
--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
 import Card from "./Card";
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
   //const [rating, setRating] = useState(8);
   //const [text, setText] = useState("This is an example of a feedback item");
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="#B000B9" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
